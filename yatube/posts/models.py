232e23ef77cafa1561b25ccf1b1be3ecc674fd1e
klo-kly <kalin.savelev07@gmail.com>
2021-11-14 21:35:28 +0300

    установлен бутсрап

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
     )
-    image = models.ImageField(upload_to="posts/", blank=True, null=True)
+    image = models.ImageField(upload_to="posts/", blank=True)
 
     def __str__(self):
         return self.text[:15]
